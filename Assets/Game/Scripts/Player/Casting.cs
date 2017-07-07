ec0822775e4915e08cae119cb95f0b209e01e161
Checkmate50 <dgeisler50@gmail.com>
2017-07-07 00:44:45 -0700

    Added simple spell casting backend

---
+++ Assets/Game/Scripts/Player/Casting.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class Casting : MonoBehaviour {
+
+  [SerializeField]
+  private string primary;
+  [SerializeField]
+  private string secondary;
+  [SerializeField]
+  private string tertiary;
+  [SerializeField]
+  private string quaternary;
+  [SerializeField]
+  private Spell[] spells;
+  [SerializeField]
+  private int maxMana;
+
+  private int mana;
+  private Vector2 hitbox;
+  private bool[] status;
+
+  private void Start() {
+    mana = maxMana;
+    hitbox = gameObject.GetComponent<BoxCollider2D>().size;
+    status = new bool[4];
+    for (int i = 0; i < status.Length; i++) {
+      status[i] = false;
+      spells[i] = Instantiate(spells[i], transform); //Just assume we have 4 spells for now
+    }
+  }
+
+  private void Update() {
+    checkFire();
+    for (int i = 0; i < spells.Length; i++)
+      castSpell(i);
+  }
+
+  private void checkFire() {
+    status[0] = checkKey(primary, status[0]);
+    status[1] = checkKey(secondary, status[1]);
+    status[2] = checkKey(tertiary, status[2]);
+    status[3] = checkKey(quaternary, status[3]);
+  }
+
+  private bool checkKey(string key, bool current) {
+    if (key == "lmb") {
+      if (Input.GetMouseButtonDown(0))
+        return true;
+      else if (Input.GetMouseButtonUp(0))
+        return false;
+    }
+    else if (key == "rmb") {
+      if (Input.GetMouseButtonDown(1))
+        return true;
+      else if (Input.GetMouseButtonUp(1))
+        return false;
+    }
+    else {
+      if (Input.GetKeyDown(key))
+        return true;
+      else if (Input.GetKeyUp(key))
+        return false;
+    }
+    return current;
+  }
+
+  public bool canFire(int spell) {
+    // Determines if we fire the weapon
+    return spells[spell].canAttack(mana);
+  }
+
+  private void castSpell(int spell) {
+    if (status[spell] && canFire(spell))
+      mana -= spells[spell].cast(transform.position + (transform.up * hitbox.y/2), transform.up, true);
+  }
+}
---
+++ Assets/Game/Scripts/Player/Spell.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Spell : MonoBehaviour {
+
+  [SerializeField]
+  protected PlayerAttack attack;
+  [SerializeField]
+  protected int cooldown;
+  [SerializeField]
+  protected int damage;
+  [SerializeField]
+  protected int cost;
+
+  protected int attackCD;
+
+  private void Start() {
+    attackCD = 0;
+  }
+
+  private void Update() {
+    if (attackCD > 0)
+      attackCD -= 1;
+  }
+
+  public bool canAttack(int mana) {
+    //Debug.Log(attackCD);
+    return cost <= mana && attackCD == 0;
+  }
+
+  public int cast(Vector3 location, Vector3 target) {
+    return cast(location, target, false);
+  }
+
+  public int cast(Vector3 location, Vector3 target, bool inDirection) {
+    attackCD = cooldown;
+    Projectile p = Instantiate(attack, location, Quaternion.identity);
+    p.setTarget(target, inDirection);
+    p.setDamage(damage);
+    return cost;
+  }
+}
